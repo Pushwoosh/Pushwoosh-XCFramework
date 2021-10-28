f76df991222940af79898fde556d61cf2191d3a2
Andrey Kiselev <akiselev@pushwoosh.com>
2021-10-28 13:52:46 +0300

    Changed checksum

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "Pushwoosh",
             url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.3.0/Pushwoosh.xcframework.zip",
-            checksum: "2f969dc888ac22c3a20b7544c46d6747fff64c24e84371034351a9c695b2eb8b"
+            checksum: "ded6fdeee62893d90cdd6c4e6393ef2b2f9792454e9b2f74add74906b145b7fc"
         )
     ]
 )
