2f4b9777f4229b2ce92baed92b8c2fe01d58b8c3
Andrey Kiselev <akiselev@pushwoosh.com>
2022-03-31 15:09:40 +0200

    release 6.4.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pushwoosh",
-            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.3.5/Pushwoosh.xcframework.zip",
-            checksum: "004138ee1626ededaba9e45b797262685807498d204847edf5355d5867cdfb9c"
+            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.4.0/Pushwoosh.xcframework.zip",
+            checksum: "f80a4df2e8002728d27f433e0346b767790d32eb34161860910f45d05b87a9ba"
         )
     ]
 )
