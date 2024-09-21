b3cd3fddd427f02a9488b092dddf8a0afe7cc727
PushwooshCI <github@pushwoosh.com>
2024-09-21 15:58:55 +0700

    Release 6.7.4

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PushwooshFramework",
-            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.7.3/PushwooshFramework.xcframework.zip",
-            checksum: "3751730cd062f6b71549e99ac82598f771e04cb1ce0194dc203add52abdfd775"
+            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.7.4/PushwooshFramework.xcframework.zip",
+            checksum: "d805e46c2c2c8bae7698150eda3513dca117060a650f963fd4823ded774ff23b"
         )
     ]
 )
