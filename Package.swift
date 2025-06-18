5763474ce2148977f7440dbb99cf7397ddb5bd48
pushwoosh <pushwoosh@pushwooshmini001.lan>
2025-06-18 23:51:58 +0700

    Release 6.9.0

--- Package.swift
+++ Package.swift
@@ -23,27 +23,27 @@ let package = Package(
         .binaryTarget(
             name: "PushwooshFramework",
             url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.9.0/PushwooshFramework.xcframework.zip",
-            checksum: "1ea2a1fd33cdbeceb1eec1f1eab202be341fad451b7f776a65a252e83acd4f91"
+            checksum: "77aad50adae8f519652b24e075f3ecda355ad446bf6ecb45292bce9e3635dfba"
         ),
         .binaryTarget(
             name: "PushwooshCore",
             url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.9.0/PushwooshCore.xcframework.zip",
-            checksum: "4656d236f7e19933f45342ef14dba0f55840db0718b568663ec9205f83b5e62d"
+            checksum: "632eeb5be5d013860c172c1945cc95d4e9e6511082366f3d01486a9343a8e7ee"
         ),
         .binaryTarget(
             name: "PushwooshBridge",
             url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.9.0/PushwooshBridge.xcframework.zip",
-            checksum: "dfd2ab91451de191f781ff1413c77569510b49d997dad3d52571fb788bc59d60"
+            checksum: "0f685d30d307a2ffd587e25cb76baf92564ef137de9723cd15e1dd5b240f33b0"
         ),
         .binaryTarget(
             name: "PushwooshLiveActivities",
             url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.9.0/PushwooshLiveActivities.xcframework.zip",
-            checksum: "3de159a1b13c33c9f4aba76570e2f12166c92d2fced072d6cfafd577d958c4a2"
+            checksum: "a553fb9832d7f3df2e1402bdccade20bec64d72e7aa96f24c65bbb4724c14a08"
         ),
         .binaryTarget(
             name: "PushwooshVoIP",
             url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.9.0/PushwooshVoIP.xcframework.zip",
-            checksum: "48e3a5837c5d15539edb416875d90160dccb4354643b2e90e7758f3a5591d1ad"
+            checksum: "5dafeeb26d43be1bfc5c967476e325ac3f107b7fa5695baf9d055e0c888df880"
         )
     ]
 )
